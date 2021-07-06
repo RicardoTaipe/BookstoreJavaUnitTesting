9d7486d0481c356c12c1389221373dd3e3f191c6
Ricardo Taipe <rdavid28@hotmail.es>
2021-07-06 10:16:08 -0500

    Add Display name in Tests

--- src/test/java/BookShelfSpec.java
+++ src/test/java/BookShelfSpec.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.time.Year;
+import java.util.*;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,13 +31,14 @@ public class BookShelfSpec {
     }
 
     @Test
-    @DisplayName("is empty when no book added")
+    @DisplayName("is empty when no book added to it")
     public void shelfEmptyWhenNoBookAdded() {
         List<Book> books = shelf.books();
         assertTrue(books.isEmpty(), "BookShelf should be empty.");
     }
 
     @Test
+    @DisplayName("bookshelf contains two books when two books are added")
     public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
         shelf.add(effectiveJava, codeComplete);
         List<Book> books = shelf.books();
@@ -45,6 +46,7 @@ public class BookShelfSpec {
     }
 
     @Test
+    @DisplayName("empty bookshelf remain empty when add is called without books")
     public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
         shelf.add();
         List<Book> books = shelf.books();
@@ -52,6 +54,7 @@ public class BookShelfSpec {
     }
 
     @Test
+    @DisplayName("bookshelf is arranged lexicographically by book title ")
     public void bookshelfArrangedByTitle() {
         shelf.add(effectiveJava, codeComplete, mythicalManMonth);
         List<Book> books = shelf.arrange();
@@ -60,6 +63,7 @@ public class BookShelfSpec {
     }
 
     @Test
+    @DisplayName("bookshelf is arranged by user provided criteria")
     public void booksInBookShelfAreInInsertionOrderAfterCallingArrange() {
         shelf.add(effectiveJava, codeComplete, mythicalManMonth);
         shelf.arrange();
@@ -69,10 +73,42 @@ public class BookShelfSpec {
     }
 
     @Test
+    @DisplayName("books inside bookshelf are grouped according to user provided criteria")
     public void bookshelfArrangedByUserProvidedCriteria() {
         shelf.add(effectiveJava, codeComplete, mythicalManMonth);
         Comparator<Book> reversed = Comparator.<Book>naturalOrder().reversed();
         List<Book> books = shelf.arrange(reversed);
         assertThat(books).isSortedAccordingTo(reversed);
+    }
+
+    @Test
+    @DisplayName("books inside bookshelf are grouped by publication year")
+    public void groupBooksInsideBookShelfByPublicationYear() {
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth);
+        Map<Year, List<Book>> booksByPublicationYear = shelf.groupByPublicationYear();
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(2008))
+                .containsValues(singletonList(effectiveJava));
+
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(2004))
+                .containsValues(singletonList(codeComplete));
+
+        assertThat(booksByPublicationYear)
+                .containsKey(Year.of(1975))
+                .containsValues(singletonList(mythicalManMonth));
+    }
+
+    @Test
+    @DisplayName("books inside bookshelf are grouped according to user provided criteria(group by author name)")
+    void groupBooksByUserProvidedCriteria() {
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth);
+        Map<String, List<Book>> booksByAuthor = shelf.groupBy(Book::getAuthor);
+        assertThat(booksByAuthor).containsKey("Joshua Bloch")
+                .containsValues(singletonList(effectiveJava));
+        assertThat(booksByAuthor).containsKey("Steve McConnel")
+                .containsValues(singletonList(codeComplete));
+        assertThat(booksByAuthor)
+                .containsKey("Frederick Phillips Brooks").containsValues(singletonList(mythicalManMonth));
     }
 }
